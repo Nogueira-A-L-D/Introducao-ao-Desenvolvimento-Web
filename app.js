0edda531772ac4815f52dc3532bca74820f4a132
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2023-07-13 18:00:51 -0300

    Places public files in folder and switches to express
    Changes css, js, and svgs files to a public folder. Changes main Node js script to use express, solving use of .css and .js files with .html ones. Adds node_module to git.ignore.

--- app.js
+++ app.js
@@ -1,58 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const server = express();
+const { readFile } = require('fs').promises;
 
-const host = 'localhost';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-    console.log('requested url: --V')
-    if (req.url.indexOf('.css') != -1) {
+server.use(express.static('public'));
 
-        fs.readFile('./styles.css', function (err, data) {
-            if (err) console.log(err);
-            res.writeHead(200, { 'Content-Type': 'text/css' });
-            res.write(data);
-            res.end();
-        });
-
-    }
-    if (req.url === '/') {
-        fs.readFile("./index.html", function (err, data) {
-            if (err) throw err;
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        });
-    }
-    else if (req.url === '/receitas/brigadeiro.html') {
-        fs.readFile('./receitas/brigadeiro.html', function (err, data) {
-            if (err) throw err;
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        });
-    }
-    else {
-        fs.readFile('./404.html', function (err, data) {
-            if (err) throw err;
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        });
-    }
-
-    if (req.url.indexOf('.js') != -1) { //req.url has the pathname, check if it conatins '.js'
-        if (req.url === '/receitas/brigadeiro.html') {
-            fs.readFile('./receitas/reviewrecipe.js', function (err, data) {
-                if (err) console.log(err);
-                res.writeHead(200, { 'Content-Type': 'text/javascript' });
-                res.write(data);
-                res.end();
-            });
-        }
-    }
-})
-
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.get('/', async (request, response) => {
+    response.send(await readFile('./index.html', 'utf8'));
 });
+
+server.get('/receitas/brigadeiro', async (request, response) => {
+    response.send(await readFile('./receitas/brigadeiro.html', 'utf8'));
+});
+
+server.use(async (request, response) => {
+    response.status(404).send(await readFile('./404.html', 'utf8'));
+});
+
+server.listen(process.env.PORT || port, () => console.log(`App available on http://localhost:${port}`));
