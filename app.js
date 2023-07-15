110ec738a39e4bc50fd67bbf391757aa4bdc1dea
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2023-07-15 13:43:48 -0300

    Creates recipe's page based on db's content
    Adds an ejs script to load a recipe based on the loaded database's content.
    Loads said content based on the URL's path from mongo db's

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const server = express();
 const { readFile } = require('fs').promises;
-
+const ejs = require('ejs');
+const uri = require("./atlas_uri");
 const port = 8080;
+
+const client = new MongoClient(uri);
+const dataBaseName = "recipe_site";
+const collectionName = "recipes";
+
+server.set('view engine', 'ejs');
 
 server.use(express.static('public'));
 
@@ -10,8 +18,16 @@ server.get('/', async (request, response) => {
     response.send(await readFile('./index.html', 'utf8'));
 });
 
-server.get('/receitas/brigadeiro', async (request, response) => {
-    response.send(await readFile('./receitas/brigadeiro.html', 'utf8'));
+server.get('/receitas*', async (request, response) => {
+    const name = request.path.split("/").pop();
+    console.log(`Connection: ${client}`);
+    upperName = name.charAt(0).toUpperCase() + name.slice(1);
+    console.log(`Name: ${upperName}`);
+    data = await client.db(dataBaseName).collection(collectionName).findOne({ nome: upperName });
+    console.log(`Found ${data.nome}`);
+    console.log(`Imagem ${data.imagem}`);
+    response.render('recipe', data);
+    //response.send(await readFile('./receitas/recipe.html', 'utf8'));
 });
 
 server.use(async (request, response) => {
@@ -19,3 +35,4 @@ server.use(async (request, response) => {
 });
 
 server.listen(process.env.PORT || port, () => console.log(`App available on http://localhost:${port}`));
+
